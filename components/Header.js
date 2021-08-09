3c463035a751cdca3a883c9841846befdf6b00f0
Renato Pavlić <renato@Renatos-MacBook-Air.local>
2021-08-09 15:10:18 +0200

    feat(Header & Icons): adds header and header-item component

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightBulbIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <header className="">
-      <h1>This is a header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Trending" Icon={LightBulbIcon} />
+        <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Collections" Icon={CollectionIcon} />
+        <HeaderItem title="Search" Icon={SearchIcon} />
+        <HeaderItem title="Account" Icon={UserIcon} />
+      </div>
       {/* Next JS image component with lazy loading */}
       <Image
         src="https://links.papareact.com/ua6"

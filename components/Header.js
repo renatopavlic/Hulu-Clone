77b927fa5fbcc0133c63df1e0aa8831c7f267188
Renato Pavlić <renato@Renatos-MacBook-Air.local>
2021-08-09 09:51:58 +0200

    feat(Global): apply background style on global state in next.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,12 @@ const Header = () => {
     <header className="">
       <h1>This is a header</h1>
       {/* Next JS image component with lazy loading */}
-      <Image src="https://links.papareact.com/ua6" width={200} height={100} />
+      <Image
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        className="object-contain"
+      />
     </header>
   );
 };

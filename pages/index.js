c570e9cc3f740932336298b4729d5202b4bc46ca
Renato Pavlić <renato@Renatos-MacBook-Air.local>
2021-08-10 13:03:57 +0200

    feat(Results): adds result component, custom tailwind breakpoint and getServerSideProps function

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -11,6 +14,26 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Nav />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const req = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  if (!req) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return { props: { results: req.results || requests.fetchTrending.url } };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // Custom css breakpoint
+        "3xl": "2000px",
+      },
+    },
   },
   variants: {
     extend: {
       animation: ["group-hover"],
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
